40c418c1b9a3b2effcfd7b20493f840dfb900e79
Pawel Kirszniok <pawelkrszniok@gmail.com>
2021-03-03 10:01:47 +0100

    extract database exception handling to a decorator

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 from configparser import ConfigParser
 from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 from.Models.User import User
 from.Models.Ticket import Ticket
 from.Models.Post import Post
 from.Models.Ticket_to_User import Ticket_to_User
+from exceptions import exception_handler
 
 
 class DatabaseService:
@@ -21,60 +21,44 @@ class DatabaseService:
         engine = create_engine(login_string, pool_pre_ping=True)
         self.session = sessionmaker(bind=engine)()
 
+    @exception_handler
     def get_user(self, id: int) -> User:
 
-        try:
-            search = self.session.query(User).filter_by(id=id).all()
-        except SQLAlchemyError:
-            self.session.rollback()
-            return None
+        search = self.session.query(User).filter_by(id=id).all()
 
         if len(search):
             return search[0]
         else:
             return None
 
+    @exception_handler
     def get_ticket(self, id: int) -> Ticket:
 
-        try:
-            search = self.session.query(Ticket).filter_by(id=id).all()
-        except SQLAlchemyError:
-            self.session.rollback()
-            return None
+        search = self.session.query(Ticket).filter_by(id=id).all()
 
         if len(search):
             return search[0]
         else:
             return None
 
+    @exception_handler
     def get_post(self, id: int) -> Post:
 
-        try:
-            search = self.session.query(Post).filter_by(id=id).all()
-        except SQLAlchemyError:
-            self.session.rollback()
-            return None
+        search = self.session.query(Post).filter_by(id=id).all()
 
         if len(search):
             return search[0]
         else:
             return None
 
+    @exception_handler
     def search_ticket(self, user: int, role: str = None) -> list[Ticket]:
 
         if role:
-            try:
-                search = self.session.query(Ticket_to_User).filer_by(user_id=user, user_role=role)
-            except SQLAlchemyError:
-                self.session.rollback()
-                return None
+            search = self.session.query(Ticket_to_User).filer_by(user_id=user, user_role=role)
 
         else:
-            try:
-                search = self.session.query(Ticket_to_User).filer_by(user_id=user)
-            except SQLAlchemyError:
-                self.session.rollback()
-                return None
+            search = self.session.query(Ticket_to_User).filer_by(user_id=user)
 
         result = []
         for item in search:
@@ -87,17 +71,12 @@ class DatabaseService:
             if tmp:
                 search_result.append(tmp)
 
-            # here should be some extra error handling or logging if we got a none result
-
         return search_result
 
+    @exception_handler
     def search_user(self, ticket: int) -> list[User]:
 
-        try:
-            search = self.session.query(Ticket_to_User).filer_by(ticket_id=ticket)
-        except SQLAlchemyError:
-            self.session.rollback()
-            return None
+        search = self.session.query(Ticket_to_User).filer_by(ticket_id=ticket)
 
         result = []
         for item in search:
@@ -114,6 +93,7 @@ class DatabaseService:
 
         return search_result
 
+    @exception_handler
     def search_post(self, user: int = None, ticket: int = None) -> list[Post]:
         """
         Get a list of posts of a particular user or belonging to a ticket. Use only one of the modes at the time.
@@ -122,57 +102,36 @@ class DatabaseService:
             return None
 
         if ticket:
-            try:
-                search = self.session.query(Post).filer_by(ticket_id=ticket)
-            except SQLAlchemyError:
-                self.session.rollback()
-                return None
+            search = self.session.query(Post).filer_by(ticket_id=ticket)
 
         else:
-            try:
-                search = self.session.query(Post).filer_by(author_id=user)
-            except SQLAlchemyError:
-                self.session.rollback()
-                return None
+            search = self.session.query(Post).filer_by(author_id=user)
 
         return search
 
+    @exception_handler
     def save_user(self, user: User):
 
-        try:
-            self.session.add(user)
-            self.session.commit()
-        except SQLAlchemyError:
-            self.session.rollback()
-            raise
+        self.session.add(user)
+        self.session.commit()
 
+    @exception_handler
     def save_ticket(self, ticket: Ticket):
 
-        try:
-            self.session.add(ticket)
-            self.session.commit()
-        except SQLAlchemyError:
-            self.session.rollback()
-            raise
+        self.session.add(ticket)
+        self.session.commit()
 
+    @exception_handler
     def save_post(self, post: Post):
 
-        try:
-            self.session.add(post)
-            self.session.commit()
-        except SQLAlchemyError:
-            self.session.rollback()
-            raise
+        self.session.add(post)
+        self.session.commit()
 
+    @exception_handler
     def save_relationship(self, user_id, ticket_id, role):
 
         rel = Ticket_to_User(user_id, ticket_id, role)
 
-        try:
-            self.session.add(rel)
-            self.session.commit()
-
-        except SQLAlchemyError:
-            self.session.rollback()
-            raise
+        self.session.add(rel)
+        self.session.commit()
 
